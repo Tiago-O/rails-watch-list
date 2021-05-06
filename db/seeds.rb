f5d7216f3c6a356cf92b81fcf330d955b4f4594a
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-06 11:29:14 +0100

    seeds are now 10 pages of the API

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'cleaning up database'
+puts 'cleaning up databases'
 Movie.destroy_all
 List.destroy_all
 Bookmark.destroy_all
-puts 'database is clean'
+puts 'databases are clean'
 
 puts 'creating movies'
 
 # API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-user_serialized = URI.open(url).read
-user = JSON.parse(user_serialized)
-results = user['results']
+base_url = 'http://tmdb.lewagon.com/movie/top_rated'
+10.times do |n|
+  puts "importing movies from page #{n + 1}"
+  url = "#{base_url}?page=#{n + 1}"
+  parsed_url = JSON.parse(open(url).read)
+  results = parsed_url['results']
 
-results.each do |movie|
-  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['backdrop_path']}", rating: movie['vote_average'])
+  results.each do |movie|
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "https://image.tmdb.org/t/p/w500/#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
 
-puts "created #{Movie.count} films"
+puts "created #{Movie.count} movies"
